25b5fc05795d9610f45b05c711939a49326a2b17
Shubham Jagdhane <shubhamjagdhane1010@gmail.com>
2023-05-07 19:00:48 +0530

    feat: #1 | added basic counter event for understanding

--- src/Tui.hs
+++ src/Tui.hs
@@ -1,9 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP #-}
+
 module Tui where
 
-import Brick
+import Lens.Micro ((^.))
+import Lens.Micro.TH (makeLenses)
+import Lens.Micro.Mtl
+import Brick.AttrMap
+import Brick.Main
+import Brick.Types
+import Brick.Widgets.Core
+import qualified Graphics.Vty as V
 
-tui :: IO()
-tui = simpleMain ui
+data CustomEvent = Counter deriving Show
 
-ui :: Widget ()
-ui = str "Hello World"
+data St = 
+    St { _stLastBrickEvent :: Maybe (BrickEvent () CustomEvent) 
+       , _stCounter :: Int
+       }
+    deriving Show
+
+
+makeLenses ''St
+
+theApp :: App St CustomEvent ()
+theApp = 
+    App { appDraw = drawUI
+        , appChooseCursor = showFirstCursor
+        , appHandleEvent = appEvent
+        , appStartEvent = return ()
+        , appAttrMap = const $ attrMap V.defAttr []
+        }
+
+
+drawUI :: St -> [Widget ()]
+drawUI st = [a]
+    where
+        a = (str $ "Last Event: " <> (show $ st ^. stLastBrickEvent))
+            <=>
+            (str $ "Counter value is:" <> (show $ st ^. stCounter))
+
+appEvent :: BrickEvent () CustomEvent -> EventM () St ()
+appEvent e = 
+    case e of
+      VtyEvent (V.EvKey V.KEsc []) -> halt
+      VtyEvent _ -> do
+          stCounter %= (+1)
+          stLastBrickEvent .= Just e
+      _ -> return ()
+
+initialState :: St
+initialState = 
+    St { _stLastBrickEvent = Nothing
+       , _stCounter = 0
+       }
+
+tui :: IO ()
+tui = do
+    endState <- defaultMain theApp initialState
+    print endState
+
