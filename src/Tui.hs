766e4316deaa392f6cea34c63acd85615b24877c
Shubham Jagdhane <shubhamjagdhane1010@gmail.com>
2023-09-17 01:22:54 +0530

    added up & down cursor movement with highlighted color

--- src/Tui.hs
+++ src/Tui.hs
@@ -3,14 +3,20 @@
 module Tui(tui) where
 
 import System.Directory
+import System.Exit
+import qualified Brick as B
 import Brick.Main
 import  Brick.Types
+import  Brick.Util
 import Brick.AttrMap
     ( attrMap
     )
 import Brick.Widgets.Core
 import qualified Graphics.Vty as V
+import Graphics.Vty.Attributes
 import Graphics.Vty.Input.Events
+import Cursor.Simple.List.NonEmpty
+import qualified Data.List.NonEmpty as NE
 
 
 tui :: IO ()
@@ -19,8 +25,8 @@ tui = do
     endState <- defaultMain tuiApp initialState
     print endState
 
-data TuiState = TuiState 
-    { tuiStatePaths :: [FilePath]
+newtype TuiState = TuiState 
+    { tuiStatePaths :: NonEmptyCursor FilePath
     } deriving (Show, Eq)
 
 type ResourceName = String
@@ -32,32 +38,50 @@ tuiApp =
         , appChooseCursor = showFirstCursor
         , appHandleEvent = handleTuiEvent
         , appStartEvent = pure ()
-        , appAttrMap = const $ attrMap V.defAttr []
+        , appAttrMap = const $ attrMap V.defAttr [(B.attrName "selected", fg red)]
         }
 
 buildInitialState :: IO TuiState
 buildInitialState = do
     here <- getCurrentDirectory
     contents <- getDirectoryContents here
-    pure TuiState { tuiStatePaths = contents }
+    case NE.nonEmpty contents of
+      Nothing -> die "There are no contents."
+      Just ne -> pure TuiState { tuiStatePaths = makeNonEmptyCursor ne }
 
 --drawTui :: TuiState -> [Widget ()]
 --drawTui _ts = str "Hello World"
 
 drawTui :: TuiState -> [Widget ResourceName]
-drawTui ts = [ vBox $ map drawPath $ tuiStatePaths ts ]
+drawTui ts = 
+    let nec = tuiStatePaths ts
+     in [ vBox $
+         concat
+            [ map (drawPath False) $ reverse $ nonEmptyCursorPrev nec
+            , [drawPath True $ nonEmptyCursorCurrent nec]
+            , map (drawPath False) $ nonEmptyCursorNext nec
+            ]
+        ]
 
-drawPath :: FilePath -> Widget n
-drawPath = str 
+drawPath :: Bool -> FilePath -> Widget n
+drawPath b = 
+    (if b 
+        then B.withAttr (B.attrName "selected")
+        else id) . 
+    str
 
 handleTuiEvent :: BrickEvent ResourceName e -> EventM ResourceName TuiState ()
 handleTuiEvent (VtyEvent (V.EvKey (V.KChar 'q') _)) = halt
-    {-
-    case e of 
-      VtyEvent vtye -> 
-          case vtye of
-            EvKey (KChar 'q') [] -> halt s
-            _ -> continue s 
-      _ -> continue s
-
--}
+handleTuiEvent (VtyEvent (V.EvKey KDown [])) = do
+    st <- get
+    let nec = tuiStatePaths st
+    case nonEmptyCursorSelectNext nec of 
+        Nothing -> continueWithoutRedraw 
+        Just nec' -> put $ TuiState { tuiStatePaths = nec' }
+handleTuiEvent (VtyEvent (V.EvKey KUp [])) = do
+    st <- get
+    let nec = tuiStatePaths st
+    case nonEmptyCursorSelectPrev nec of 
+        Nothing -> continueWithoutRedraw 
+        Just nec' -> put $ TuiState { tuiStatePaths = nec' }
+handleTuiEvent _= halt
